b206cf1e2820696836d6d1aedc72f345f2017b5e
unknown <Dustin.Guillen14@yahoo.com>
2020-12-15 17:00:28 -0700

    countdown timer functionality set for New Years 2021

---
+++ countdown-timer/script.js
@@ -0,0 +1,18 @@
+let countDownDate = new Date('January 1, 2021 10:00:00').getTime();
+
+let x = setInterval(function () {
+
+    let curtime = new Date().getTime();
+
+    let distance = countDownDate - curtime;
+
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / (1000));
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+})
